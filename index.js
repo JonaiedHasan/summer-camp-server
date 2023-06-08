de51204bcf8ff81d142f35f55159631f5b9b5a22
Alvihub <jonaiedhasan120@gmail.com>
2023-06-09 04:23:51 +0600

    Set the server

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xdyi47s.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+ 
+    const classesCollection = client.db("sportsDB").collection("classes");
+    const instructorsCollection = client.db("sportsDB").collection("instructors");
+    const selectedClassCollection = client.db("sportsDB").collection("selectedClass");
+
+
+// get classes
+app.get('/classes' , async(req,res) =>{
+    const result = await classesCollection.find().toArray();
+    res.send(result)
+})
+// get instructors
+app.get('/instructors' , async(req,res) =>{
+    const result = await instructorsCollection.find().toArray();
+    res.send(result)
+})
+
+// select classes
+app.get('/selectedClass', async (req, res) => {
+    const email = req.query.email;
+    console.log(email)
+
+    if (!email) {
+      res.send([]);
+    }
+
+    const query = { email: email };
+    const result = await selectedClassCollection.find(query).toArray();
+    res.send(result)
+  })
+
+
+app.post('/selectedClass', async(req, res) =>{
+    const item = req.body;
+    console.log(item);
+    const result = await selectedClassCollection.insertOne(item);
+    res.send(result)
+})
+
+app.delete('/selectedClass/:id', async(req, res) =>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await selectedClassCollection.deleteOne(query);
+  res.send(result)
+})
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('summer camp is sitting')
+})
+
+
+app.listen(port, () => {
+    console.log(`summer camp is sitting on post ${port}`);
+})
