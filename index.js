fe541c425af44122d18462bb22a2366f413a8976
Alvihub <jonaiedhasan120@gmail.com>
2023-06-10 23:57:54 +0600

    set manage user api

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
- 
+
     const classesCollection = client.db("sportsDB").collection("classes");
     const instructorsCollection = client.db("sportsDB").collection("instructors");
     const selectedClassCollection = client.db("sportsDB").collection("selectedClass");
@@ -39,58 +39,96 @@ async function run() {
 
 
     // users api
-    app.post('/users', async(req, res) =>{
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+
+
+    // make admin
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+    // make instructor
+    app.put('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+
+
+
+
+
+
+    app.post('/users', async (req, res) => {
       const user = req.body;
       console.log(user);
-      const query ={email: user.email}
+      const query = { email: user.email }
       const remainingUser = await usersCollection.findOne(query);
       if (remainingUser) {
-        return res.send({message: 'This user already exists'})
+        return res.send({ message: 'This user already exists' })
       }
       const result = await usersCollection.insertOne(user);
       res.send(result)
     })
 
 
-// get classes
-app.get('/classes' , async(req,res) =>{
-    const result = await classesCollection.find().toArray();
-    res.send(result)
-})
-// get instructors
-app.get('/instructors' , async(req,res) =>{
-    const result = await instructorsCollection.find().toArray();
-    res.send(result)
-})
+    // get classes
+    app.get('/classes', async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result)
+    })
+    // get instructors
+    app.get('/instructors', async (req, res) => {
+      const result = await instructorsCollection.find().toArray();
+      res.send(result)
+    })
 
-// select classes
-app.get('/selectedClass', async (req, res) => {
-    const email = req.query.email;
-    console.log(email)
+    // select classes
+    app.get('/selectedClass', async (req, res) => {
+      const email = req.query.email;
+      console.log(email)
 
-    if (!email) {
-      res.send([]);
-    }
+      if (!email) {
+        res.send([]);
+      }
 
-    const query = { email: email };
-    const result = await selectedClassCollection.find(query).toArray();
-    res.send(result)
-  })
+      const query = { email: email };
+      const result = await selectedClassCollection.find(query).toArray();
+      res.send(result)
+    })
 
 
-app.post('/selectedClass', async(req, res) =>{
-    const item = req.body;
-    console.log(item);
-    const result = await selectedClassCollection.insertOne(item);
-    res.send(result)
-})
+    app.post('/selectedClass', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await selectedClassCollection.insertOne(item);
+      res.send(result)
+    })
 
-app.delete('/selectedClass/:id', async(req, res) =>{
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)};
-  const result = await selectedClassCollection.deleteOne(query);
-  res.send(result)
-})
+    app.delete('/selectedClass/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedClassCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
@@ -107,10 +145,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('summer camp is sitting')
+  res.send('summer camp is sitting')
 })
 
 
 app.listen(port, () => {
-    console.log(`summer camp is sitting on post ${port}`);
+  console.log(`summer camp is sitting on post ${port}`);
 })
