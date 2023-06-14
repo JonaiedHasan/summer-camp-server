97af853bbe19d5a2087dfccd6d2ae886f66208fd
Alvihub <jonaiedhasan120@gmail.com>
2023-06-14 19:41:12 +0600

    clear cors commit

--- index.js
+++ index.js
@@ -389,14 +389,3 @@ app.listen(port, () => {
 
 
 
-
-
-
-
-
-// const corsConfig = {
-//   origin: '*',
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE']
-//   }
-//   app.use(cors(corsConfig))
